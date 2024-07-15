267fa3ca1906db053782d7df0075476419a9a31c
saimoniasty <siamon8732@gmail.com>
2024-07-15 16:12:14 +0200

    walone pierwiastki

--- funkcyje/l_konczyPierwiastkiem/konPierwiastek.cpp
+++ funkcyje/l_konczyPierwiastkiem/konPierwiastek.cpp
@@ -3,8 +3,8 @@
 #include <string>
 using namespace std;
 
-bool konczy_pierwiastkiem(string n,int n1){
-    int pierwiastek=sqrt(n1);
+bool konczy_pierwiastkiem(string n,float n1){
+    float pierwiastek=sqrt(n1);
 
     string pierwiastek1=to_string(pierwiastek);
     string odwrocony_pierwiastek;
@@ -14,10 +14,17 @@ bool konczy_pierwiastkiem(string n,int n1){
     for(int i=dlugosc_pierwiastka-1;i>=0;i--){
         odwrocony_pierwiastek+=pierwiastek1[i];
     }
-
-    for(int i=n.length()-1;i>=dlugosc_pierwiastka;i--){
-        if(n[i]==odwrocony_pierwiastek[j]) dobre++;
-        j++;
+    if(n1/2==dlugosc_pierwiastka){
+        for(int i=n.length()-1;i>=dlugosc_pierwiastka;i--){
+            if(n[i]==odwrocony_pierwiastek[j]) dobre++;
+            j++;
+        }
+    }
+    else{
+        for(int i=n.length()-1;i>=dlugosc_pierwiastka-1;i--){
+            if(n[i]==odwrocony_pierwiastek[j]) dobre++;
+            j++;
+        }
     }
     if(dobre==dlugosc_pierwiastka) return 1;
     else return 0;
@@ -25,7 +32,7 @@ bool konczy_pierwiastkiem(string n,int n1){
 
 int main(){
     string x;
-    int x1;
+    float x1;
     cout<<"Sprawdz czy liczba konczy sie swoim pierwiastkiem"<<endl;
     cout<<"Podaj x: "; cin>>x;
     x1=atoi(x.c_str());
