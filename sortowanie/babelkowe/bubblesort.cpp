03c51357e2a49afa74cd3edc8c06531e5d59eef2
saimoniasty <siamon8732@gmail.com>
2024-08-02 17:27:56 +0200

    babelki

---
+++ sortowanie/babelkowe/bubblesort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void sortowanie_babelkowe(int *tab,int n){
+    for(int i=1;i<n;i++){
+        for(int j=n-1;j>=1;j--){
+            if(tab[j]<tab[j-1]){
+                int bufor; bufor=tab[j-1];
+                tab[j-1]=tab[j];
+                tab[j]=bufor;
+            }
+        }
+    }
+}
+
+int main(){
+    int ile;
+    cout<<"Sortowanie babelkowe"<<endl;
+
+    cout<<"Ile losowych liczb w tablicy: "; cin>>ile;
+
+    int *tablica;
+    tablica=new int [ile];
+
+    srand(time(NULL));
+
+    for(int i=0;i<ile;i++){
+        tablica[i]=rand()%100000+1;
+        cout<<tablica[i]<<" ";
+    }
+
+    sortowanie_babelkowe(tablica,ile);
+
+    cout<<endl;
+
+    for(int i=0;i<ile;i++){
+        cout<<tablica[i]<<" ";
+    }
+
+    delete [] tablica;
+
+    return 0;
+}
